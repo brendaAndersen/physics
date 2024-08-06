783ad68595e6ab1b005284d11e59f39620b1adbc
brenda <brenda_andersen@protonmail.com>
2024-08-05 22:36:26 -0300

    updated backend and frontend

--- physics/src/pages/auth/sign-in.tsx
+++ physics/src/pages/auth/sign-in.tsx
@@ -6,24 +6,36 @@ import { useForm } from "react-hook-form"
 import { z } from "zod";
 import { toast } from "sonner";
 import { Link } from "react-router-dom";
+import { signIn } from "../../api/sign-in";
 
 const signInForm = z.object({
   email: z.string().email(),
+  password: z.string().min(6),
 })
 
 type SignInForm = z.infer<typeof signInForm>
 
 export function SignIn() {
   const { register, handleSubmit, formState: { isSubmitting }} = useForm<SignInForm>();
-  
+ const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
   async function handleSignIn(data: SignInForm){
     try{
       console.log(data)
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await authenticate({ email: data.email, password: data.password })
+      // await new Promise((resolve) => setTimeout(resolve, 2000));
   
-      toast.success('An email with a auth link was send to you!', {
+      // toast.success('An email with a auth link was send to you!', {
+      //   action: {
+      //     label: "Resend",
+      //     onClick: () => handleSignIn(data)
+      //   },
+      // })
+      
+      toast.success('Success!', {
         action: {
-          label: "Resend",
+          label: "Ok!",
           onClick: () => handleSignIn(data)
         },
       })
@@ -71,6 +83,27 @@ export function SignIn() {
               E-mail
             </label>
             </div>
+             <div className="relative z-0 w-full mb-5 group">
+            <input
+              {...register("password")}
+              name="password"
+              type="password"
+              id="password"
+              className="block py-2.5 px-0 w-full text-sm  bg-transparent 
+          border-0 border-b-2 appearance-none text-white 
+          border-gray-600 dark:focus:border-violet-250 focus:outline-none 
+          focus:ring-0 focus:border-violet-250 peer font-thin
+          "
+              placeholder=" "
+              required
+            />
+            <label
+              className="text-violet-250 font-bold 
+          peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-violet-250 peer-focus:dark:text-violet-250 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-bold"
+            >
+              Password
+            </label>
+            </div>
             </div>
             <Button disabled={isSubmitting} className="bg-sky-500 hover:bg-sky-700" type="submit">Login</Button>
           </form>
@@ -79,3 +112,7 @@ export function SignIn() {
     </>
   );
 }
+function useMutation(arg0: { mutationFn: ({ email, password }: import("../../api/sign-in").SignInBody) => Promise<void>; }): { mutateAsync: any; } {
+  throw new Error("Function not implemented.");
+}
+
--- physics/src/pages/auth/sign-up.tsx
+++ physics/src/pages/auth/sign-up.tsx
@@ -10,6 +10,7 @@ import { Link, useNavigate } from "react-router-dom";
 const signUpForm = z.object({
     name: z.string(),
     email: z.string().email(),
+    password: z.string().min(6),
 })
 
 type SignUpForm = z.infer<typeof signUpForm>
@@ -85,6 +86,27 @@ export function SignUp() {
           peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-violet-250 peer-focus:dark:text-violet-250 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-bold"
             >
               E-mail
+            </label>
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+            <input
+              {...register("password")}
+              name="password"
+              type="password"
+              id="password"
+              className="block py-2.5 px-0 w-full text-sm  bg-transparent 
+          border-0 border-b-2 appearance-none text-white 
+          border-gray-600 dark:focus:border-violet-250 focus:outline-none 
+          focus:ring-0 focus:border-violet-250 peer font-thin
+          "
+              placeholder=" "
+              required
+            />
+            <label
+              className="text-violet-250 font-bold 
+          peer-focus:font-medium absolute text-sm duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-violet-250 peer-focus:dark:text-violet-250 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 text-bold"
+            >
+              Password
             </label>
             </div>
             <Button disabled={isSubmitting} className="bg-sky-500 hover:bg-sky-700" type="submit">Create</Button>
